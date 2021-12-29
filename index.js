968e666a43d464ccd17a7324a6a55e7bb2298759
= <rtyenriques@gmail.com>
2021-12-29 00:20:56 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+// const multiply () => {
+//    let result num1 * num2
+
+// }
+
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 64
+let num4 = 10
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,20);
